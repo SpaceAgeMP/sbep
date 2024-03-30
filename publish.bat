479eae5ad01d08db00fe800aef924b4b755b32fe
Doridian <git@doridian.net>
2024-03-30 12:44:50 -0700

    Add icon

---
+++ publish.bat
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\gmad" create -folder .\ -out ..\sbep.gma
+"C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\gmpublish" update -id 2361916825 -addon ..\sbep.gma -icon icon.jpg
+del  ..\sbep.gma
